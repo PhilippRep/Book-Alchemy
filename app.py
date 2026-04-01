1576f6893151ad494d9ae114ff04c2456442bab8
Philipp <philipp_repke@gmx.de>
2026-04-01 14:35:27 +0200

    added new route for home and vreating a new row in table author

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 import os
 from data_models import db, Author, Book
 
@@ -10,6 +10,26 @@ db.init_app(app)
 
 with app.app_context():
   db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('home.html'), 200
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method== 'POST':
+        name = request.form.get('name')
+        birthdate = request.form.get('birthdate')
+        date_of_death = request.form.get('date_of_death')
+        new_author = Author(name = name,
+               birth_of_date = birthdate,
+               date_of_death = date_of_death)
+        db.session.add(new_author)
+        db.session.commit()
+        print(f"Author:{name} successfully created")
+        return redirect(url_for('home'))
+
+    return render_template('add_author.html')
 
 
 if __name__ == "__main__":
