dfa5fa41da54ddbb6980e206eb28679fc2d53a8f
Philipp <philipp_repke@gmx.de>
2026-04-01 14:02:30 +0200

    created a database and defined two modells for one table each

---
+++ data_models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import *
+
+db = SQLAlchemy()
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    birth_date = db.Column(db.Date)
+    date_of_death = db.Column(db.Date, nullable=True)
+
+    def __repr__(self):
+        return f"Author(id = {self.id}, name = {self.name})"
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String(17), unique=True, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Book(id = {self.id}, title = {self.title})"
+
